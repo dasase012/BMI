8016cf15f819417b3075aed2654aa96420161b93
SNU1 <SNU1@Bohee>
2018-09-19 16:34:25 +0900

    text sample

---
+++ BMI/src/practice/Connect.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Connect {
+
+	/*
+	 깃허브 
+	 javaLab2/API-ArrayList/src/com/sist/data/DataManager_valueOf문자열화시켜줌.java
+	 참고해서 텍스트 파일 파싱.
+	 혹은 JSOUP 으로 텍스트 파싱.
+	 */
+}
