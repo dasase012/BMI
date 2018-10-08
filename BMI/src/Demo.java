53bf63610279f03c38db2f0f4bf1d7193e091f97
SNU1 <SNU1@Bohee>
2018-10-08 16:40:53 +0900

    add coordinate

--- BMI/src/Demo.java
+++ BMI/src/Demo.java
@@ -1,69 +1,60 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.*;
 import javax.imageio.ImageIO;
  
-public class Demo extends JPanel implements ActionListener, KeyListener{
+public class Demo extends JPanel {
  
    BufferedImage image;
-   Ellipse2D.Double circle, circle2, circle3, circle4;
+   Ellipse2D.Double circle;
    Rectangle rect;
-   Dimension size = new Dimension();			
-   /*Rectangle, Dimension이 갖고 있는 함수로 좌표 (10,10)으로 한정 짓거나 
-   x,y 좌표대로 이미지나 점이 움직이도록 설정 가능할 듯.*/
    
-   Timer t = new Timer(5, this);
-   int x=400, y=250, 
-      x0=100, y0=100,
-      x2=800, y2=800,
-      velx=0, vely=0;
+   static List<Integer> integers = new ArrayList<>();
+   static int handX,handY,circleX,circleY;
    
    Demo(BufferedImage image) {
+
       this.image = image;
         rect = new Rectangle(image.getWidth(), image.getHeight());
         
-      t.start();
-      addKeyListener(this);
+      //t.start();
+      //addKeyListener(this);
       setFocusable(true);
       setFocusTraversalKeysEnabled(false);
+	  
+      
    }
+   public static void getFileVariables (int x, int y,int cx,int cy) {
+		 handX =  integers.get(x);
+		 handY = integers.get(y);
+		 circleX =  integers.get(cx);
+		 circleY= integers.get(cy);
+	     System.out.println(x+"///"+y+"///"+cx+"///"+cy);
+	   }
    
    protected void paintComponent(Graphics g) {
       super.paintComponent(g);
-      
+   //   Demo d = new Demo(image);
       /*draw circles*/
       Graphics2D g2 = (Graphics2D)g;
       
       initCircle();
-   
-      // Draw image centered.
-      /*rect.x = (size.width - rect.width)/2;
-      rect.y = (size.height - rect.height)/2;*/
-      
-      /*rect.x=(int) (rect.getWidth()/2);
-      rect.y=(int) (rect.getHeight()/2);*/
-      rect.x = x;
-      rect.y = y;
-      
-      
-      
-      g2.drawImage(image, rect.x, rect.y, this);
+ 
+     // rect.x = integers.get(1)*100;
+      System.out.println(this.handX+"\n");
+     rect.y = handY*100;
+       
+      g2.drawImage(image,  this.handX*100, this.handY*100, this);
       g2.setPaint(Color.RED);
-
       g2.fill(circle);
-     /* g2.fill(circle2);
-      g2.fill(circle3);
-      g2.fill(circle4);*/
       
    }
 
@@ -71,77 +62,66 @@ public class Demo extends JPanel implements ActionListener, KeyListener{
    private void initCircle() {
                
       /*Edge dots(x,y,40,40)=>dot position and size*/            
-      circle = new Ellipse2D.Double(x0,y0,40,40);
-      /*circle2 = new Ellipse2D.Double(x0,y2,40,40);
-      circle3 = new Ellipse2D.Double(x2,y0,40,40);
-      circle4 = new Ellipse2D.Double(x2,y2,40,40);*/
-      
+      circle = new Ellipse2D.Double(circleX*100,circleY*100,40,40);
+     
    }
 
-   public void actionPerformed(ActionEvent e) {
-      //infinite motion
-      repaint();
-      //velocity up (motion activate)
-      x += velx;
-      y += vely;
-   }
-
-   public void keyPressed(KeyEvent e) {
-      int code = e.getKeyCode();
-      if(code == KeyEvent.VK_UP) {
-         velx = 0;
-         vely = -1;
-      }
-      if(code == KeyEvent.VK_DOWN) {
-         velx = 0;
-         vely = 1;
-      }
-      if(code == KeyEvent.VK_LEFT) {
-         velx = -1;
-         vely = 0;
-      }
-      if(code == KeyEvent.VK_RIGHT) {
-         velx = 1;
-         vely = 0;
-      }
-   }
-
-   public void keyReleased(KeyEvent e) {
-      //Stopping the motion when key released
-      velx=0;
-      vely=0;
-   }
-
-   public void keyTyped(KeyEvent e) {}
-   
-   
    
    public static void main(String[] arguments) throws IOException {
-   
-      //insert image
-       BufferedImage image = ImageIO.read(new File("image/point.png"));
-       Demo d = new Demo(image);
-   
-       //main window
-       JFrame frame = new JFrame();
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-       frame.add(new JScrollPane(d));
-   
-       //main setup
-       frame.setSize(1000, 1000);
-       frame.setLocation(500, 50);
-       frame.setVisible(true);
-       
-       //read text file
-       File file = new File("C:\\Users\\SNU1\\git\\BMI\\BMI\\text\\test.txt"); 
 
-       BufferedReader br = new BufferedReader(new FileReader(file)); 
+	   //read text file
+		   try {
+		
+			   File file = new File("C:\\Users\\SNU1\\git\\BMI\\BMI\\text\\test.txt");
+			   String[] splitedStr = null;
+			   FileReader filereader = new FileReader(file);
+			   BufferedReader bf = new BufferedReader(filereader);
+			   String line = "";
+			   splitedStr = null;
+			   
+			   long length = 0;
+			   while((line=bf.readLine())!=null){
+				   if(line.isEmpty()) {
+					   break;
+				   }
+				   splitedStr = line.split(" ");
+				   
+				   for(int i=1;i<splitedStr.length;i++) {
+					   splitedStr[i]=splitedStr[i].trim();
+					   System.out.println(splitedStr[i]);
+					   integers.add(i);
+				   }
+			   }
+			   bf.close();
+			} catch (FileNotFoundException e) {
+					e.printStackTrace();
+			}
 
-       String st; 
-       while ((st = br.readLine()) != null) 
-        System.out.println(st); 
-        
-    
-  }
+		  
+		 getFileVariables(integers.get(6),integers.get(7),integers.get(8),integers.get(9));
+	      
+		  
+		  System.out.println(handX +" ?????"+handY+" ?????"+circleX +" ?????"+circleY);
+		  System.out.println(integers.get(6)+" " +integers.get(7)+" "+integers.get(8)+" "+integers.get(9));
+	   
+	      //insert image
+	       BufferedImage image = ImageIO.read(new File("image/point.png"));
+	       Demo d = new Demo(image);
+	   
+	       //main window
+	       JFrame frame = new JFrame();
+	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+	       frame.add(new JScrollPane(d));
+	   
+	       //main setup
+	       frame.setSize(1000, 1000);
+	       frame.setLocation(500, 50);
+	       frame.setVisible(true);
+	       
+	       
+		 
+	  }
+
+
    
 }
--- BMI/src/practice/Connect.java
+++ BMI/src/practice/Connect.java
@@ -1,20 +1,27 @@
 package practice;
 
-import java.io.*; 
-//Java Program to illustrate reading from FileReader 
-//using BufferedReader 
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Connect 
 { 
 public static void main(String[] args)throws Exception 
 { 
 File file = new File("C:\\Users\\SNU1\\git\\BMI\\BMI\\text\\test.txt"); 
-
-BufferedReader br = new BufferedReader(new FileReader(file)); 
-StringTokenizer tokenizer;
-String st; 
-while ((st = br.readLine()) != null) 
- System.out.println(st); 
+Scanner scanner = new Scanner(file);
+List<Integer> integers = new ArrayList<>();
+while (scanner.hasNext()) {
+    if (scanner.hasNextInt()) {
+        integers.add(scanner.nextInt());
+    } 
+    else {
+        scanner.next();
+    }
+}
+System.out.println(integers);
 } 
+
+
 } 
